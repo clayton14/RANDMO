4378ae501eddeeb625e761ba9323053cc800326b
clayton14 <claytoneasley77@gmail.com>
2022-01-28 14:42:13 -0500

    might make this it's own project

--- src/HW.py
+++ src/HW.py
@@ -1,25 +1,11 @@
 import wikipedia, os, sys, random
 from wikipedia import PageError
 from wikipedia import DisambiguationError
+import warnings
 #TODO : make a more well rounded web scraping tool 
 # add GUI maye
 # add google docs support
 
-
-def main(search_terms, num_sentance, output="out.txt"):
-    with open(os.path.join(os.getcwd(), output), 'r+') as f:
-        try:
-            for i, term  in enumerate(search_terms):
-                searchs = wikipedia.search(term)
-                for search in searchs:
-                    summary = wikipedia.summary(search, sentences=num_sentance)
-                    f.writelines(term + ": " + summary + "\n\n\n")
-                    print("DONE: " + search)
-        except PageError and DisambiguationError as e:
-            print(e)
-            print(f"[ERROR] {term}, searching for related information")
-
-        print("=====research complete=====")
     
             
 
@@ -30,19 +16,20 @@ def search_test():
         print(wikipedia.summary(terms), end='\n')
 
 
-def test(search_terms, num_sentance, output="out.txt"):
-    num_terms = len(search_terms) 
-    while num_sentance > 0:
-        try:
-            wikipedia.summary(search_terms[num_sentance])
-            num_terms -= 1
-            print("DONE: " + search_terms[num_sentance])
-        except PageError as e:
-            print(f"[ERROR] could not find page for {search_terms[num_terms]}")
-            print(e)
-            num_terms -= 1
+def main(search_terms, num_sentance, output="out.txt"):
+    assert len(search_terms) != 0, "List is empty"
+    warnings.catch_warnings()
+    warnings.simplefilter("ignore")
+    with open(output, "w") as f:
+        for term in search_terms:
+            try:
+                    summary = wikipedia.summary(term,sentences=num_sentance)
+                    print(f"[DONE]: {term}")
+                    f.writelines(f"{term}:\n{summary}\n\n")
+            except (DisambiguationError,  PageError) as e:
+                print(f"[ERROR:{term}] page not found for {term}! try and make your search terms more specific")
+                continue
 
-        print("=====research complete=====")
 
 #listh of this to to look up example x = ["minecraft", "Jacob Riis"]
 # x = [ 
@@ -53,7 +40,7 @@ def test(search_terms, num_sentance, output="out.txt"):
 # ]
 
 x = [
-    "Edward Bellamy","Suburban", "steel frame", "sprawl", "public transit", "Great Wave", 
+    "gjdlskgj","Edward Bellamy","Suburban", "steel frame", "Urban sprawl", "public transit", 
 ]
-test(x, 3, output="out.txt")
+main(x, 3, output="out.txt")
 #earch_test()
