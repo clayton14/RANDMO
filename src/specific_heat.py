5a4d9d90c7cee4fa05876a9d5cc8c6beaa27ed05
clayton14 <claytoneasley77@gmail.com>
2022-01-31 09:25:04 -0500

    ok

--- src/specific_heat.py
+++ src/specific_heat.py
@@ -1,5 +1,29 @@
-import sys
+import sys, os
+
+delta = '\u0394'
+
+
+def main() -> float:
+    while True:
+        # Q = float(input(""))
+        # C = float(input(""))
+        # Ti = float(input(""))
+        # Tf = float(input(""))
+        choice = input(f"[1] Q\n[2] C \n[3] {delta}T\nsloving for what: ")
+        choice = int(choice)
+        if (choice > 3 and not isinstance(choice, int)):
+            print(f"[ERROR] {choice} is not an option")
+            continue
+        if(choice == 1):
+            C = float(input("Enter the specific heat (j/gC): "))
+            M = float(input("Enter the mass (g): "))
+            Ti = float(input("Enter the init temp (C): "))
+            Tf = float(input("Enter the final temp (C): "))
+            return M * C * abs(Tf - Ti)
+
 
 
 if __name__ == "__main__":
-    pass
+    answer = main()
+    print(answer)
+   
